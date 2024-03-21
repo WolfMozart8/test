ae1c94dddf2a7370fcbe26bf12c03dfa56f55d3e
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2024-03-21 00:51:04 -0300

    added list

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+const textList = document.getElementById("text-list")
+
 const textarea = document.querySelector("#textarea")
 const timeStart = document.querySelector("#time-start")
 const timeEnd = document.querySelector("#time-end")
 
 const btnAdd = document.querySelector("#btn-add")
 
-const list = JSON.parse(sessionStorage.getItem("list"))
+const list = []
+if (sessionStorage.getItem("list")) {
+    list = JSON.parse(sessionStorage.getItem("list"))
+}
 
 btnAdd.addEventListener("click", () => {
     addText(
@@ -15,15 +20,23 @@ btnAdd.addEventListener("click", () => {
 })
 
 function addText (text, start, end) {
-    if (end < start) throw Error("invalid values")
+    // if (end < start) {throw Error("invalid values")}
 
     const newText = {
         text, start, end
     }
 
     list.push(newText)
+    domAddText(newText)
 
     textarea.value = ""
-    console.log(list);
     sessionStorage.setItem("list", JSON.stringify(list))
+}
+
+function domAddText(textObject) {
+    const domTextDIv = document.createElement("div")
+    domTextDIv.classList.add("text")
+    domTextDIv.textContent = textObject.text
+
+    textList.appendChild(domTextDIv)
 }
