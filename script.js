a1dc03939ea0a766b31de3cb3976ddd00cc6182b
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2024-03-21 16:57:15 -0300

    video buttons listeners

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
   // VIDEO
   let video = document.getElementById("myVideo");
+  // VIDEO BUTTONS
   const videoInput = document.getElementById("videoInput");
+  const btnShow = document.getElementById("btn-show");
+  const btnPress = document.getElementById("btn-press");
+  const btnPlay = document.getElementById("btn-play");
+  const btnPause = document.getElementById("btn-pause");
+  const btnRepeat = document.getElementById("btn-repeat");
+  const btnPrev2 = document.getElementById("btn-prev2");
+  const btnPrev5 = document.getElementById("btn-prev5");
+  // VIDEO TIME
+  let currentTime = 0
+  let startTime = 0
+  let endTime
 
   videoInput.addEventListener("change", (e) => {
     const file = e.target.files[0];
@@ -17,13 +29,16 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     video.src = fileUrl;
   });
-  video.addEventListener("loadeddata", (e) => console.log(e.target.duration));
+  video.addEventListener("loadeddata", (e) => console.log(e));
 
   let list = [];
   if (sessionStorage.getItem("list")) {
     list = JSON.parse(sessionStorage.getItem("list"));
   }
 
+  /**
+   * Add new text
+   */
   btnAdd.addEventListener("click", () => {
     addText(textarea.value, timeStart.value, timeEnd.value);
   });
@@ -55,4 +70,58 @@ document.addEventListener("DOMContentLoaded", (e) => {
   /**
    * VIDEO
    */
+  video.addEventListener("timeupdate", () => {
+    currentTime = video.currentTime
+    console.log(currentTime);
+  })
+
+  // buttons
+  btnShow.addEventListener("click", () => {
+    video.classList.toggle("show")
+  })
+
+  btnPress.addEventListener("mousedown", () => {
+    startTime = currentTime
+  })
+  btnPress.addEventListener("mouseup", () => {
+    endTime = currentTime
+    console.log(startTime, endTime);
+  })
+
+  // player
+  btnPlay.addEventListener("click", () => {
+    video.play()
+  })
+  btnPause.addEventListener("click", () => {
+    video.pause()
+  })
+  // prev seconds
+  btnPrev2.addEventListener("click", () => {
+    video.pause()
+    const newCurrentTime = currentTime - 2
+
+    if (newCurrentTime > 0) {
+      video.currentTime = newCurrentTime
+      video.play()
+    } else {
+      video.currentTime = 0
+      video.play()
+    }
+
+  })
+
+  btnPrev5.addEventListener("click", () => {
+    video.pause()
+    const newCurrentTime = currentTime - 5
+
+    if (newCurrentTime > 0) {
+      video.currentTime = newCurrentTime
+      video.play()
+    } else {
+      video.currentTime = 0
+      video.play()
+    }
+
+  })
+
 });
